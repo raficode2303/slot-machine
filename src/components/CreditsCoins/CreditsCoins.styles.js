945dd87ff6b705e5b09007129813018d4c0cec29
rafi&pw-mosh <rafi-mosh@email.com>
2022-08-03 10:50:00 +0300

    updated

---
+++ src/components/CreditsCoins/CreditsCoins.styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  background-color: black;
+  width: 100%;
+  height: 100%;
+`
+const Content = styled.div`
+  text-transform: uppercase;
+  color: red;
+  width: 33%;
+  background-color: inherit;
+`
