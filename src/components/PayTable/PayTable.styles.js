b72e490c9b88abd2577e885c1f3509fda70ec534
rafi&pw-mosh <rafi-mosh@email.com>
2022-08-17 10:39:02 +0300

    PayTabel version1.0 - Done!, still strugle with the vertical carousel(slot machine)

---
+++ src/components/PayTable/PayTable.styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Table = styled.table`
+  width: 50%;
+  text-align: center;
+  background-image: linear-gradient(red, white, blue);
+  border: 1px solid;
+  border-collapse: collapse;
+`
+
+export const Tr = styled.tr`
+  border: 1px solid;
+`
+export const Th = styled.th`
+  border: 1px solid;
+`
+export const Td = styled.td`
+  border: 1px solid;
+`
--- src/components/PayTable/index.js
+++ src/components/PayTable/index.js
@@ -1,5 +1,48 @@
 import React from 'react'
+import { Table, Tr, Th, Td } from './PayTable.styles'
+
+const payTableArray = [
+  {
+    comb: '7 7 7',
+    prize: 40000,
+  },
+  {
+    comb: '🪙 🪙 🪙',
+    prize: 4000,
+  },
+  {
+    comb: 'same same same',
+    prize: 400,
+  },
+  {
+    comb: 'any two 🍒',
+    prize: 40,
+  },
+  {
+    comb: 'any one 🍒',
+    prize: 4,
+  },
+]
 
 export const PayTable = () => {
-  return <div>PayTable</div>
+  return (
+    <Table>
+      <Tr>
+        <Th></Th>
+        <Th>1st</Th>
+        <Th>2nd</Th>
+        <Th>3rd</Th>
+        <Th></Th>
+      </Tr>
+      {payTableArray.map((payLine) => (
+        <Tr key={payLine.comb}>
+          <Td>{payLine.comb}</Td>
+          <Td>{payLine.prize}</Td>
+          <Td>{payLine.prize * 2}</Td>
+          <Td>{payLine.prize * 5}</Td>
+          <Td>{payLine.comb}</Td>
+        </Tr>
+      ))}
+    </Table>
+  )
 }
